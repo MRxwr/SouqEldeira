b58f2fc283b4bb06ce52c1c5b970f9f8259701f1
MRxwr <nasserhatab@gmail.com>
2024-12-22 11:06:51 +0300

    Update bladeFastAdd.php

--- admin/views/bladeFastAdd.php
+++ admin/views/bladeFastAdd.php
@@ -56,7 +56,7 @@ if( isset($_POST["enTitle"]) ){
 }
 ?>
 <div class="row">
-<div class="col-sm-4">
+<div class="col-sm-12">
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
@@ -127,7 +127,7 @@ if( isset($_POST["enTitle"]) ){
 </div>
 </div>
 
-<div class="col-sm-8">
+<div class="col-sm-12">
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left" style="width: 100%;">
