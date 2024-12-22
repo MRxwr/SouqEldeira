cd6ce24f451cd92a85b694071343c5bc30dc0aa5
MRxwr <nasserhatab@gmail.com>
2024-12-22 11:06:39 +0300

    Update bladeFastAdd.php

--- admin/views/bladeFastAdd.php
+++ admin/views/bladeFastAdd.php
@@ -3,8 +3,7 @@ if( isset($_POST["enTitle"]) ){
 	if( isset($_POST["id"]) && !empty($_POST["id"])){
 		$product = selectDB("products","`id` = '{$_POST["id"]}'");
 		$updateArray = array(
-			"categoryId" => "{$_POST["categoryId"][0]}",
-			"brandId" => "{$_POST["brandId"]}",
+			"categoryId" => "{$_POST["categoryId"]}",
 			"arTitle" => ($_POST["arTitle"]),
 			"enTitle" => ($_POST["enTitle"]),
 			"arDetails" => ($_POST["arDetails"]),
@@ -13,28 +12,6 @@ if( isset($_POST["enTitle"]) ){
 			"cost" => "{$_POST["cost"]}",
 		);
 		updateDB("products",$updateArray,"`id` LIKE '{$_POST["id"]}'");
-		deleteDB("category_products","`productId` = {$_POST["id"]}");
-		for( $i =0; $i < sizeof($_POST["categoryId"]) ; $i++ ){
-			$data = array(
-				"productId" => $_POST["id"],
-				"categoryId" => $_POST["categoryId"][$i],
-			);
-			insertDB("category_products",$data);
-		}
-		if( $product[0]["type"] == 1 ){
-			$oldId = selectDB("attributes_products","`productId` = '{$_POST["id"]}'");
-			deleteDB("attributes_products","`productId` = {$_POST["id"]}");
-			$dataInsert = array(
-				"productId" => $_POST["id"],
-				"price" => "{$_POST["price"]}",
-				"cost" => "{$_POST["cost"]}",
-				"quantity" => "{$_POST["quantity"]}",
-				"sku" => "{$_POST["sku"]}"
-			);
-			insertDB("attributes_products",$dataInsert);
-			$newId = selectDB("attributes_products","`productId` = '{$_POST["id"]}'");
-			updateDB("cart",array("attributeId" => $newId[0]["id"]),"`attributeId` = '{$oldId[0]["id"]}'");
-		}
 		for( $i = 0; $i < sizeof($_FILES['logo']['tmp_name']); $i++ ){
 			if( is_uploaded_file($_FILES['logo']['tmp_name'][$i]) ){
 				$filenewname = uploadImageBanner($_FILES["logo"]["tmp_name"][$i]);
@@ -49,22 +26,19 @@ if( isset($_POST["enTitle"]) ){
 		}
 		$data = array(
 			'categoryId' => $_POST["categoryId"],
-			'brandId' => $_POST["brandId"],
 			'enTitle' => $_POST["enTitle"],
 			'arTitle' => $_POST["arTitle"],
 			'enDetails' => $_POST["enDetails"],
 			'arDetails' => $_POST["arDetails"],
 			'price' => $_POST["price"],
 			'cost' => $_POST["cost"],
-			'quantity' => $_POST["quantity"],
-			'sku' => $_POST["sku"],
 		);
 		foreach ($files as $key => $file) {
 			$data["logo[$key]"] = $file;
 		}
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => 'https://coeoapp.com/requests/dashboard/index.php?a=Product&action=add',
+			CURLOPT_URL => "{$baseURL}requests/dashboard/index.php?a=Product&action=add",
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => '',
 			CURLOPT_MAXREDIRS => 10,
@@ -86,7 +60,7 @@ if( isset($_POST["enTitle"]) ){
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-	<h6 class="panel-title txt-dark"><?php echo direction("Product Express","المنتج سريع") ?></h6>
+	<h6 class="panel-title txt-dark"><?php echo direction("Ads Details","تفاصيل الاعلان") ?></h6>
 </div>
 	<div class="clearfix"></div>
 </div>
@@ -96,23 +70,9 @@ if( isset($_POST["enTitle"]) ){
 		<div class="row m-0">
             <div class="col-md-12">
 			<label><?php echo direction("Category","القسم") ?></label>
-				<select name="categoryId[]" class="selectpicker" data-style="form-control btn-default btn-outline" multiple required>
+				<select name="categoryId" class="selectpicker" data-style="form-control btn-default btn-outline" required>
 					<?php
                     if( $categories = selectDB("categories","`status` = '0' AND `hidden` = '1'") ){
-                        for( $i = 0; $i < sizeof($categories); $i++ ){
-                            $title = direction($categories[$i]["enTitle"],$categories[$i]["arTitle"]);
-                            echo "<option value='{$categories[$i]["id"]}'>{$title}</option>";
-                        }
-                    }
-                    ?>
-				</select>
-			</div>
-
-			<div class="col-md-12">
-			<label><?php echo direction("Brand","الماركة") ?></label>
-				<select name="brandId" class="selectpicker" data-style="form-control btn-default btn-outline" required>
-					<?php
-                    if( $categories = selectDB("brands","`status` = '0' AND `hidden` = '1'") ){
                         for( $i = 0; $i < sizeof($categories); $i++ ){
                             $title = direction($categories[$i]["enTitle"],$categories[$i]["arTitle"]);
                             echo "<option value='{$categories[$i]["id"]}'>{$title}</option>";
@@ -131,25 +91,10 @@ if( isset($_POST["enTitle"]) ){
 			<label><?php echo direction("Arabic Title","العنوان بالعربي") ?></label>
 			<input type="text" name="arTitle" class="form-control" value="" required>
 			</div>
-			
-			<div class="col-md-6">
-			<label><?php echo direction("Cost","التكلفة") ?></label>
-			<input type="float" step="any" min="0"  name="cost" class="form-control" value="0">
-			</div>
 
 			<div class="col-md-6">
 			<label><?php echo direction("Price","القيمة") ?></label>
 			<input type="float" step="any" min="0" name="price" class="form-control" value="0" >
-			</div>
-			
-			<div class="col-md-6">
-			<label><?php echo direction("Quantity","الكمية") ?></label>
-			<input type="number" step="1" min="0" name="quantity" class="form-control" value="0">
-			</div>
-			
-			<div class="col-md-6">
-			<label><?php echo direction("SKU","SKU") ?></label>
-			<input type="text" name="sku" class="form-control" required>
 			</div>
 
 			<div class="col-md-12">
@@ -186,7 +131,7 @@ if( isset($_POST["enTitle"]) ){
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left" style="width: 100%;">
-	<h6 class="panel-title txt-dark"><?php echo direction("Products List","قائمة المنتجات") ?></h6>
+	<h6 class="panel-title txt-dark"><?php echo direction("Ads List","قائمة الاعلانات") ?></h6>
 	</div>
 		<div class="clearfix"></div>
 	</div>
@@ -209,12 +154,9 @@ if( isset($_POST["enTitle"]) ){
 		if( $products = selectDB("products","`status` = '0' AND `hidden` != '2' AND `enDetails` LIKE '' ORDER BY `id` DESC") ){
 			for( $i = 0; $i < sizeof($products); $i++ ){
 				if($image = selectDB("images","`productId` = '{$products[$i]["id"]}' ORDER BY `id` ASC")){
-
 				}else{
 					$image[0]["imageurl"] = "noimage.png";
 				}
-				$attributes = selectDB("attributes_products","`productId` = '{$products[$i]["id"]}' ORDER BY `id` ASC LIMIT 1");
-				$categories = selectDB("category_products","`productId` = '{$products[$i]["id"]}'");
 			if ( $products[$i]["hidden"] == 2 ){
 				$icon = "fa fa-eye";
 				$link = "?v={$_GET["v"]}&show={$products[$i]["id"]}";
@@ -237,12 +179,6 @@ if( isset($_POST["enTitle"]) ){
 					  </button>
 					  <ul class="dropdown-menu">
 						<?php
-						  if ( $products[$i]["type"] == 0 ){
-							echo '<li><a href="?v=AttributesProducts&id='.$products[$i]["id"].'"><i class="fa fa-sitemap"></i></a></li>';
-						  }
-						  if ( $products[$i]["collection"] == 1 ){
-							echo '<li><a href="?v=Collection&id='.$products[$i]["id"].'"><i class="fa fa-object-group"></i></a></li>';
-						  }
 						  echo '<li><a id="'.$products[$i]["id"].'" class="edit" href="javascript:void(0)"><i class="zmdi zmdi-edit"></i></a></li>';
 						  if ( $products[$i]["hidden"] == 0 ){
 							echo '<li><a href="includes/products/delete.php?id='.$products[$i]["id"].'"><i class="fa fa-eye-slash"></i></a></li>';
@@ -269,13 +205,9 @@ if( isset($_POST["enTitle"]) ){
 					<div style="display: none;">
 						<label id="enDetails<?php echo $products[$i]["id"]?>"><?php echo $products[$i]["enDetails"] ?></label>
 						<label id="arDetails<?php echo $products[$i]["id"]?>"><?php echo $products[$i]["arDetails"] ?></label>
-						<label id="brandId<?php echo $products[$i]["id"]?>"><?php echo $products[$i]["brandId"] ?></label>
-						<label id="price<?php echo $products[$i]["id"]?>"><?php echo $attributes[0]["price"] ?></label>
-						<label id="cost<?php echo $products[$i]["id"]?>"><?php echo $attributes[0]["cost"] ?></label>
-						<label id="sku<?php echo $products[$i]["id"]?>"><?php echo $attributes[0]["sku"] ?></label>
-						<label id="quantity<?php echo $products[$i]["id"]?>"><?php echo $attributes[0]["quantity"] ?></label>
+						<label id="price<?php echo $products[$i]["id"]?>"><?php echo $products[$i]["price"] ?></label>
 						<label id="image<?php echo $products[$i]["id"]?>"><?php echo json_encode($image)?></label>
-						<label id="category<?php echo $products[$i]["id"]?>"><?php echo json_encode($categories) ?></label>
+						<label id="category<?php echo $products[$i]["id"]?>"><?php echo $categories ?></label>
 					</div>
 				</td>
 			</tr>
@@ -303,18 +235,11 @@ if( isset($_POST["enTitle"]) ){
 		$("input[name=arTitle]").val($("#arTitle"+id).html());
 		tinymce.get('enDetails').setContent($("#enDetails"+id).html());
 		tinymce.get('arDetails').setContent($("#arDetails"+id).html());
-		$("input[name=cost]").val($("#cost"+id).html());
-		$("input[name=quantity]").val($("#quantity"+id).html());
 		$("input[name=price]").val($("#price"+id).html());
-		$("input[name=sku]").val($("#sku"+id).html());
 		$("select[name=brandId] option").prop("selected", false);
 		$("select[name=brandId]").val($("#brandId"+id).html()).selectpicker('refresh');
-		$("select[name='categoryId[]'] option").prop("selected", false);
-		var data = JSON.parse($("#category"+id).html());
-		$.each(data, function(index, value){
-			$("select[name='categoryId[]'] option[value='"+value["categoryId"]+"']").prop("selected", true);
-		});
-		$("select[name='categoryId[]']").selectpicker('refresh');
+		$("select[name=categoryId] option").prop("selected", false);
+		$("select[name=categoryId]").val($("#categoryId"+id).html()).selectpicker('refresh');
 		$("#images").empty().attr("style","margin-top:10px;display:block"); // Clear the div
 		$.each(JSON.parse($("#image"+id).html()), function(index, value){
 			var img = $("<img>").attr({
