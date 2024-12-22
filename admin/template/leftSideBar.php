915f6d87868f5f6b5d4677bc0a0893d13041c219
MRxwr <nasserhatab@gmail.com>
2024-12-22 10:47:55 +0300

    Update leftSideBar.php

--- admin/template/leftSideBar.php
+++ admin/template/leftSideBar.php
@@ -5,7 +5,7 @@
 			<i class="zmdi zmdi-more"></i>
 		</li>
 <?php 
-if( $pages = selectDB("pages","`status` = '0' AND `section` = '0'") ){
+if( $pages = selectDB("pages","`status` = '0' AND `section` = '0' ORDER BY `rank` ASC") ){
 	if( $roles = selectDB("roles","`id` = '{$userType}'") ){
 		$list = json_decode($roles[0]["pages"],true);
 	}else{
