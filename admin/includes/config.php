454839f2e6abf21fd346b11aff16bd073dbab4c7
MRxwr <nasserhatab@gmail.com>
2024-12-22 10:18:05 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,11 +6,11 @@ $config['v'] = strtotime("now");
 $config['theme'] = "default";
 
 $servername = "localhost";
-$usernameDB = "u800348178_myacad2USER";
+$usernameDB = "u671249433_souqeldeiraU";
 $password = "N@b$90949089";
-$dbname = "u800348178_myacadv2DB";
-$baseURL = "https://myacad.app/requests";
-$printImageUrl = "https://myacad.app";
+$dbname = "u671249433_souqeldeiraD";
+$baseURL = "https://souqeldeira.createkwservers.com/";
+$printImageUrl = "https://souqeldeira.createkwservers.com/";
 $dbconnect = new MySQLi($servername,$usernameDB,$password,$dbname);
 if ( $dbconnect->connect_error ){
 	die("Connection Failed: " .$dbconnect->connect_error );
