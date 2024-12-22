47a0f47b4ba4d6a45f5ef00cd8cba0e36dfa3840
MRxwr <nasserhatab@gmail.com>
2024-12-22 12:19:28 +0300

    Update bladeShops.php

--- admin/views/bladeShops.php
+++ admin/views/bladeShops.php
@@ -133,7 +133,7 @@ if( isset($_POST["updateRank"]) ){
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-<h6 class="panel-title txt-dark"><?php echo direction("List of Shops","قائمة المحلات") ?></h6>
+<h6 class="panel-title txt-dark"><?php echo direction("List of Offices","قائمة المكاتب") ?></h6>
 </div>
 <div class="clearfix"></div>
 </div>
