49712e6fc7f0f5ac9a485f466a03d478d39683d4
MRxwr <nasserhatab@gmail.com>
2024-12-24 13:34:25 +0300

    Update bladeFAQ.php

--- views/bladeFAQ.php
+++ views/bladeFAQ.php
@@ -1,57 +1,41 @@
-
-		<div class="row"> 
-		<div class="col-md-11 mx-auto">
-			
-			<div class="form-container form-container-faq">
-		
-				<div class="mb-4 text-center">
-					<img src="assets/img/logo-1.png" class="img-fluid" alt="...">
-				</div>
-		        
-				<div class="mb-4 text-center">
-					<h5 class="faq-title" ><?php echo Trans('app','Frequently asked questions about properties for rent or sale in Kuwait'); ?></h5> 
-			    </div>
-		
-		        <div class="accordion accordion-flush" id="accordionFlushExample">
-				  <div class="accordion-item">
-				    <h2 class="accordion-header" id="flush-headingOne">
-				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-				        <?php echo Trans('app','How many real estate ads are currently for rent or for saleIn Kuwait on the website'); ?>
-				      </button>
-				    </h2>
-				    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-				      <div class="accordion-body"><?php echo Trans('app','The number of real estate ads offered for rent or for sale in Kuwait is 4883 new ads'); ?></div>
-				    </div>
-				  </div>
-				  <div class="accordion-item">
-				    <h2 class="accordion-header" id="flush-headingTwo">
-				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-				         <?php echo Trans('app','How many real estate ads are currently for rent or for saleIn Kuwait on the website'); ?>
-				      </button>
-				    </h2>
-				    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-				      <div class="accordion-body"><?php echo Trans('app','The number of real estate ads offered for rent or for sale in Kuwait is 4883 new ads'); ?></div>
-				    </div>
-				  </div>
-				  <div class="accordion-item">
-				    <h2 class="accordion-header" id="flush-headingThree">
-				      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-				         <?php echo Trans('app','How many real estate ads are currently for rent or for saleIn Kuwait on the website'); ?>
-				      </button>
-				    </h2>
-				    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-				      <div class="accordion-body"><?php echo Trans('app','The number of real estate ads offered for rent or for sale in Kuwait is 4883 new ads'); ?></div>
-				    </div>
-				  </div>
-				</div>
-				
-				  
-			     <div class="text-center mt-4"> 
-			      	<p class="add-ad-form-text fw-bold"><?php echo Trans('app','Send us message by'); ?> <a href=""><?php echo Trans('app','WhatsApp'); ?></a> <?php echo Trans('app','or'); ?> <a href="contact.php"><?php echo Trans('app','call us'); ?></a> <?php echo Trans('app','for help'); ?></p>
-			     </div>
-
+<?php
+$socialMedia = selectDB("s_media","`id` = '1'");
+?>
+<div class="row">
+<div class="col-md-11 mx-auto">
+	<div class="form-container form-container-faq">
+		<div class="mb-4 text-center">
+			<img src="assets/img/logo-1.png" class="img-fluid" alt="...">
+		</div>
+		<div class="mb-4 text-center">
+			<h5 class="faq-title" ><?php echo Trans('app','Frequently asked questions about properties for rent or sale in Kuwait'); ?></h5> 
+		</div>
+		<div class="accordion accordion-flush" id="accordionFlushExample">
+			<?php
+			if( $faq = selectDB("faq","`id` != '0' AND `status` = '0' ORDER BY `rank` ASC") ){
+				for( $i = 0; $i < count($faq); $i++ ){
+					?>
+					<div class="accordion-item">
+					<h2 class="accordion-header" id="flush-heading<?php echo $i ?>">
+						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $i ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $i ?>">
+							<?php echo direction($faq[$i]["enQuestion"],$faq[$i]["arQuestion"]) ?>
+						</button>
+					</h2>
+					<div id="flush-collapse<?php echo $i ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $i ?>" data-bs-parent="#accordionFlushExample">
+						<div class="accordion-body"><?php echo direction($faq[$i]["enAnswer"],$faq[$i]["arAnswer"]) ?></div>
+					</div>
+					</div>
+					<?php
+				}
+			}
+			?>
+		</div>
+			<div class="text-center mt-4"> 
+			<p class="add-ad-form-text fw-bold"><?php echo Trans('app','Send us message by'); ?>
+			<a href="https://wa.me/<?php echo $socialMedia['mobile']; ?>"><?php echo Trans('app','WhatsApp'); ?></a>
+			<?php echo Trans('app','or'); ?>
+			<a href="?v=Contact"><?php echo Trans('app','call us'); ?></a> <?php echo Trans('app','for help'); ?></p>
 			</div>
-			
-			
-		</div>
-		</div>
+	</div>
+</div>
+</div>
