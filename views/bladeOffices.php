f17e2f4ae1fbbe7f19136be76096d3e5d1bd94d7
MRxwr <nasserhatab@gmail.com>
2024-12-24 10:49:49 +0300

    save

--- views/bladeOffices.php
+++ views/bladeOffices.php
@@ -1,99 +1,46 @@
-
-        	
-	      
-			
-			
-			
-			<div class="offices-list"> 
-			 
-			 
-			  
-			  <div class="row d-flex align-items-stretch gy-3 gx-2">
-			  	
-		  	   <div class="col-md-12"> 
-					<div class="start-page-title text-center mb-md-4 py-md-3 mt-1 mt-md-0">
-						<h4 class="mb-0"><?php echo Trans('app','Offices List'); ?></h4>
-			 		</div>
-			   </div>
-				
-			   <div class="col-6 col-lg-4">
-			  		<div class="card card-office text-center">
-					  <a class="card-img" href="?v=OfficeView">
-					  	<img src="assets/img/offices/office-1-2.png" class="img-fluid">
-					  </a>
-					  <div class="card-body">
-					    <h5 class="card-title fw-bold"><a href="?v=OfficeView"><?php echo Trans('app','Saleh al-Attal real estate company'); ?></a></h5>
-					    <div class="card-social">
-					    	<ul>
-					    		<li><a href=""><i class="bi bi-facebook"></i></a></li>
-					    		<li><a href=""><i class="bi bi-twitter-x"></i></a></li>
-					    		<li><a href=""><i class="bi bi-instagram"></i></a></li>
-					    		<li><a href=""><i class="bi bi-envelope"></i></a></li>
-					    	</ul>
-					    </div>
-					    <div class="mx-0"> 
-					    	<a href="#" class="btn btn-primary w-100"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
-					    </div>
-					  </div>
-					</div>
-				</div>
-				
-			   <div class="col-6 col-lg-4">
-			  		<div class="card card-office text-center">
-					  <a class="card-img" href="?v=OfficeView">
-					  	<img src="assets/img/offices/office-1-1.png" class="img-fluid">
-					  </a>
-					  <div class="card-body">
-					    <h5 class="card-title fw-bold"><a href="?v=OfficeView"><?php echo Trans('app','Saleh al-Attal real estate company'); ?></a></h5>
-					    <div class="mx-0">
-					    	<a href="#" class="btn btn-primary w-100"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
-					    </div>
-					  </div>
-					</div>
-				</div>
-				
-				<div class="col-6 col-lg-4">
-			  		<div class="card card-office text-center">
-					  <a class="card-img" href="?v=OfficeView">
-					  	<img src="assets/img/offices/office-1-2.png" class="img-fluid">
-					  </a>
-					  <div class="card-body">
-					    <h5 class="card-title fw-bold"><a href="?v=OfficeView"><?php echo Trans('app','Saleh al-Attal real estate company'); ?></a></h5>
-					    <div class="card-social">
-					    	<ul>
-					    		<li><a href=""><i class="bi bi-facebook"></i></a></li>
-					    		<li><a href=""><i class="bi bi-twitter-x"></i></a></li>
-					    		<li><a href=""><i class="bi bi-instagram"></i></a></li>
-					    		<li><a href=""><i class="bi bi-envelope"></i></a></li>
-					    	</ul>
-					    </div>
-					    <div class="mx-0"> 
-					    	<a href="#" class="btn btn-primary w-100"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
-					    </div>
-					  </div>
-					</div>
-				</div>
-				
-			   <div class="col-6 col-lg-4">
-			  		<div class="card card-office text-center">
-					  <a class="card-img" href="?v=OfficeView">
-					  	<img src="assets/img/offices/office-1-1.png" class="img-fluid">
-					  </a>
-					  <div class="card-body">
-					    <h5 class="card-title fw-bold"><a href="?v=OfficeView"><?php echo Trans('app','Saleh al-Attal real estate company'); ?></a></h5>
-					    <div class="mx-0">
-					    	<a href="#" class="btn btn-primary w-100"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
-					    </div>
-					  </div> 
-					</div>
-				</div>
-				
-				
-				
-				
-				
-				
-			  </div>
-			  
-			  
+<div class="offices-list"> 
+	<div class="row d-flex align-items-stretch gy-3 gx-2">
+		<div class="col-md-12"> 
+			<div class="start-page-title text-center mb-md-4 py-md-3 mt-1 mt-md-0">
+				<h4 class="mb-0"><?php echo Trans('app','Offices List'); ?></h4>
 			</div>
+		</div>
+		<?php 
+		if ( $offices = selectDB("shops","`id` != '0' AND `status` = '0' ")){
+			for( $i = 0; $i < sizeof($offices); $i++ ){
+				?>
+				<div class="col-6 col-lg-4">
+					<div class="card card-office text-center">
+						<a class="card-img" href="?v=OfficeView&id=<?php echo $offices[$i]["id"]; ?>">
+						<img src="logos/<?php echo $offices[$i]["logo"]; ?>" class="img-fluid">
+						</a>
+						<div class="card-body">
+						<h5 class="card-title fw-bold"><a href="?v=OfficeView&id=<?php echo $offices[$i]["id"]; ?>"><?php echo direction($offices[$i]["enTitle"],$offices[$i]["arTitle"]); ?></a></h5>
+						<div class="card-social">
+							<ul>
+						<?php 
+						$arraySMIcons = ["bi bi-facebook","bi bi-twitter-x","bi bi-instagram","bi bi-envelope"];
+						$arrayLinks = ["https://www.facebook.com/","https://www.twitter.com/","https://www.instagram.com/","mailto:"]; 
+						$arraySM = ["facebook","twitter","instagram","email"];
+						for( $j = 0; $j < sizeof($arraySMIcons); $j++ ){
+							if( !empty($offices[$i][$arraySM[$j]]) ){
+							?>
+							<li><a href="<?php echo $arrayLinks[$j] . $offices[$i][$arraySM[$j]]; ?>"><i class="<?php echo $arraySMIcons[$j]; ?>"></i></a></li>
+							<?php
+							}
+						}
+						?>
+							</ul>
+						</div>
+						<div class="mx-0"> 
+							<a href="tel:<?php echo $offices[$i]["phone"]; ?>" class="btn btn-primary w-100"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
+						</div>
+						</div>
+					</div>
+				</div>
+				<?php
+			}
+		}
+		?>
+	</div>
+</div>
