903a8e9be4feb32150156428a165af2bbd67f007
MRxwr <nasserhatab@gmail.com>
2024-12-24 10:13:58 +0300

    Update bladeHome.php

--- views/bladeHome.php
+++ views/bladeHome.php
@@ -177,15 +177,15 @@
 		    	<h3 class="fw-bold text-center mb-5"><?php echo Trans('app','Real estate offices in Kuwait'); ?></h3> 
 		        	
 		        <div class="owl-carousel owl-carousel-offices owl-theme">
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-1.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-2.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-3.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-1.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-2.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-3.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-1.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-2.png"></a></div>
-				    <div class="item"><a href="office-view.php"><img src="assets/img/offices/office-3.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-1.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-2.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-3.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-1.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-2.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-3.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-1.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-2.png"></a></div>
+				    <div class="item"><a href="?v=OfficeView"><img src="assets/img/offices/office-3.png"></a></div>
 				</div>
 				</div>
 				
