0bb84652b3c140d340113c3661d3ce90915544d3
MRxwr <nasserhatab@gmail.com>
2024-12-24 12:25:45 +0300

    Update translate.php

--- admin/includes/translate.php
+++ admin/includes/translate.php
@@ -1,5 +1,6 @@
 <?php
 $settingsLang = (isset($_COOKIE["CREATEkwLANG"]) && $_COOKIE["CREATEkwLANG"] == "en") ? "en" : "ar";
+$directionHTML = (isset($_COOKIE["CREATEkwLANG"]) && $_COOKIE["CREATEkwLANG"] == "en") ? "" : "rtl";
 var_dump($_COOKIE["CREATEkwLANG"]);
 if ( isset($_GET["lang"]) ){
 	$arrayLangs = ["en","ar"];
