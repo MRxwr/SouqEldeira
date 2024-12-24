0fcf5982b1fd2d798a666eb70af97aed03e187cd
MRxwr <nasserhatab@gmail.com>
2024-12-24 10:57:12 +0300

    Update bladeOfficeView.php

--- views/bladeOfficeView.php
+++ views/bladeOfficeView.php
@@ -1,63 +1,73 @@
+<?php
+if ( $offices = selectDBNew("shops",[$_GET["id"]],"`id` = ? AND `status` = '0'","")){
 
-        	
-	        <div class="row">
-	        	
-	        	<div class="col-lg-4">
-	        		<div class="office-profile-side with-white-bg px-md-4 py-md-4">
-	        			
-	        			<div class="card card-office text-center">
-						  <a class="card-img" href="office-view.php">
-						  	<img src="assets/img/offices/office-1-2.png" class="img-fluid">
-						  </a>
-						  <div class="card-body">
-						    <h5 class="card-title fw-bold"><a href="office-view.php"><?php echo Trans('app','Saleh al-Attal real estate company'); ?></a></h5>
-						    <div class="card-social">
-						    	<ul>
-						    		<li><a href=""><i class="bi bi-facebook"></i></a></li>
-						    		<li><a href=""><i class="bi bi-twitter-x"></i></a></li>
-						    		<li><a href=""><i class="bi bi-instagram"></i></a></li>
-						    		<li><a href=""><i class="bi bi-envelope"></i></a></li>
-						    	</ul>
-						    </div>
-						    <div class="mx-0"> 
-						    	<a href="#" class="webURL w-100">
-						    		<i class="bi bi-link-45deg mx-2"></i> 
-						    		<span>www.example.com</span>
-				    			</a>
-						    </div>
-						    
-						    <div class="office-side-desc">  
-							    <div class="office-description my-3"> 
-							    	<h3 class="mb-2"><?php echo Trans('app','Description'); ?></h3>
-							    	<p><?php echo Trans('app','office-description'); ?></p> 
-							    </div>
-							    <div class="mx-0"> 
-							    	<a href="#" class="btn btn-primary px-4"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
-							    </div>
-						    </div>
-						    
-						  </div>
-						</div> 
-						
-	        		</div>
-	        	</div>
-	        	
-	        	<div class="col-lg-8">
-	        		
-	        		<div class="with-white-bg px-md-4 px-2 py-4 mt-md-0 mt-4"> 
-		        		<div class="list-title mb-3"> 
-							<h4><?php echo Trans('app','Office Ads'); ?></h4> 
-						</div>
-						
-						<div class="ads-section">
-						
-						    <?php include 'themes/'.$config['theme'].'/sections/ads-main-list.php'; ?>
-						    <div class="d-block text-end mt-3">
-						  	<a href="" class="btn btn-primary"><?php echo Trans('app','More'); ?> <i class="bi bi-three-dots"></i></a>
-						    </div>
-					  
-					    </div>
-					</div> 
-					
+}else{
+	$message = direction("Office not found","المكتب غير موجود");
+	?>
+	<script>
+		alert("<?php echo $message; ?>");
+		window.location = "?v=Offices";
+	</script>
+	<?php
+}
+?>
+<div class="row">
+	<div class="col-lg-4">
+		<div class="office-profile-side with-white-bg px-md-4 py-md-4">
+			<div class="card card-office text-center">
+				<a class="card-img" href="office-view.php">
+				<img src="logos/<?php echo $offices[0]["logo"]; ?>" class="img-fluid">
+				</a>
+				<div class="card-body">
+				<h5 class="card-title fw-bold"><a href="?v=OfficeView&id=<?php echo $offices[0]["id"]; ?>"><?php echo direction($offices[0]["enTitle"],$offices[0]["arTitle"]); ?></a></h5>
+				<div class="card-social">
+					<ul>
+				<?php 
+				$arraySMIcons = ["bi bi-facebook","bi bi-twitter-x","bi bi-instagram","bi bi-envelope"];
+				$arrayLinks = ["https://www.facebook.com/","https://www.twitter.com/","https://www.instagram.com/","mailto:"]; 
+				$arraySM = ["facebook","twitter","instagram","email"];
+				for( $j = 0; $j < sizeof($arraySMIcons); $j++ ){
+					if( !empty($offices[0][$arraySM[$j]]) ){
+					?>
+					<li><a href="<?php echo $arrayLinks[$j] . $offices[0][$arraySM[$j]]; ?>"><i class="<?php echo $arraySMIcons[$j]; ?>"></i></a></li>
+					<?php
+					}
+				}
+				?>
+					</ul>
+				</div>
+				<div class="mx-0"> 
+					<a href="<?php echo $offices[0]["url"]; ?>" class="webURL w-100">
+						<i class="bi bi-link-45deg mx-2"></i> 
+						<span><?php echo $offices[0]["url"]; ?></span>
+					</a>
+				</div>
+				
+				<div class="office-side-desc">  
+					<div class="office-description my-3"> 
+						<h3 class="mb-2"><?php echo Trans('app','Description'); ?></h3>
+						<p><?php echo direction($offices[0]["enDetails"],$offices[0]["arDetails"]); ?></p> 
+					</div>
+					<div class="mx-0"> 
+						<a href="tel:<?php echo $offices[0]["mobile"]; ?>" class="btn btn-primary px-4"><i class="bi bi-telephone-fill mx-2"></i><?php echo Trans('app','Call'); ?></a>
+					</div>
+				</div>
+				</div>
+			</div> 
+		</div>
+	</div>
+	
+	<div class="col-lg-8">
+		<div class="with-white-bg px-md-4 px-2 py-4 mt-md-0 mt-4"> 
+			<div class="list-title mb-3"> 
+				<h4><?php echo Trans('app','Office Ads'); ?></h4> 
+			</div>
+			<div class="ads-section">
+				<?php include 'template/adsMainList.php'; ?>
+				<div class="d-block text-end mt-3">
+				<a href="?v=AdsList" class="btn btn-primary"><?php echo Trans('app','More'); ?> <i class="bi bi-three-dots"></i></a>
 				</div>
 			</div>
+		</div> 
+	</div>
+</div>
