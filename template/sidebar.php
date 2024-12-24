05aec175cb72f44cd6cab2574c707d6ce057932f
MRxwr <nasserhatab@gmail.com>
2024-12-24 12:42:02 +0300

    Update sidebar.php

--- template/sidebar.php
+++ template/sidebar.php
@@ -5,10 +5,10 @@
 			<div class="element1-start">
 				<div class="change-language">
 				    <?php if( $_SESSION['lang'] ==='en') { ?>   
-		        		<a class='language p-0' href='<?php echo str_replace("?lang=ar", "" ,str_replace("&lang=ar", "", $_SERVER['REQUEST_URI'])) ?>'><img src='assets/img/lang/ar.png' class='img-fluid langimg'><span><?php echo str_replace("?lang=en", "" ,str_replace("&lang=en", "", $_SERVER['REQUEST_URI'])) ?></span></a> 
+		        		<a class='language p-0' href='<?php echo getSign() . "lang=ar" ?>'><img src='assets/img/lang/ar.png' class='img-fluid langimg'><span>عربى</span></a> 
 					<?php } ?>
 					<?php if( $_SESSION['lang'] ==='ar') { ?> 	
-						<a class='language p-0' href='<?php echo str_replace("?lang=en", "" ,str_replace("&lang=en", "", $_SERVER['REQUEST_URI'])) ?>'><img src='assets/img/lang/en.png' class='img-fluid langimg'><span><?php echo str_replace("?lang=en", "" ,str_replace("&lang=en", "", $_SERVER['REQUEST_URI'])) ?></span></a>
+						<a class='language p-0' href='<?php echo getSign() . "lang=en" ?>'><img src='assets/img/lang/en.png' class='img-fluid langimg'><span>English</span></a>
 		        	<?php } ?>  
 			    </div>
 			    <div id="dismiss" class="siderbar-dismiss text-end">
@@ -18,6 +18,7 @@
 		    <?php if($_SESSION['valid']){ ?>
 		    <div class="sidebar-header">
 		    	<div class="profile">
+		    		
 		    		<div class="avatar">
 			    		<a href="?v=Home" class="d-block">
 			    			<img src="assets/img/profile.png" class="img-fluid" alt="...">
