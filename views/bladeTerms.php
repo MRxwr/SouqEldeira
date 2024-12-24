40b52efd32c334b7d5144f9d7ee624fcf72b9757
MRxwr <nasserhatab@gmail.com>
2024-12-24 13:11:52 +0300

    Update bladeTerms.php

--- views/bladeTerms.php
+++ views/bladeTerms.php
@@ -1,15 +1,13 @@
-	
-			
-			<div class="row"> 
-				
-				<div class="col-md-12"> 
-					<div class="start-page-title with-white-bg text-center mb-0 py-3">
-						<h4 class="mt-3"><?php echo Trans('app','Terms & Conditions'); ?></h4>  
-						
-						<div class="px-3 pt-3">   
-							<p><?php echo Trans('app','about-txt-1'); ?></p>
-						</div> 
-			 		</div>
-				</div>
-				
-			</div>
+<?php
+$settings = selectDB("settings","`id` = '1'");
+?>
+<div class="row"> 
+	<div class="col-md-12"> 
+		<div class="start-page-title with-white-bg text-center mb-0 py-3">
+			<h4 class="mt-3"><?php echo Trans('app','Terms & Conditions'); ?></h4>  
+			<div class="px-3 pt-3">   
+				<p><?php echo direction($settings[0]["enTerms"],$settings[0]["arTerms"]); ?></p>
+			</div> 
+		</div>
+	</div>
+</div>
