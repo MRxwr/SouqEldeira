61ce60e292e0f09ee68acb51bdec1a0d7ecbb8e5
MRxwr <nasserhatab@gmail.com>
2024-12-25 23:39:07 +0300

    Update bladeHome.php

--- views/bladeHome.php
+++ views/bladeHome.php
@@ -2,7 +2,7 @@
 	<i class="bi bi-exclamation-circle"></i> <?php echo Trans('app','A message from the manager to manage the website'); ?>
 </div>  
 
-<form class="" action="search-view.php">
+<form class="" action="?v=SearchView" method="POST">
 	<div class="search-area-row"> 
 		<div class="row">
 			<div class="main-radio-btn">
