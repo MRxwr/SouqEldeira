e94e390b5d3ac7054671873c8c9e819740edb7db
MRxwr <nasserhatab@gmail.com>
2024-12-26 09:22:37 +0300

    save

--- includes/checksouthead.php
+++ includes/checksouthead.php
@@ -7,6 +7,7 @@ if ( isset($_COOKIE[$cookieSession]) && !empty($_COOKIE[$cookieSession]) ){
 	session_start ();
 	$svdva = $_COOKIE[$cookieSession];
 	if ( $user = selectDBNew("users", [$svdva], "`keepMeAlive` LIKE ? AND `hidden` != '2' AND `status` = '0'", "")){
+        $_SESSION['valid'] = true;
         $userDetails = array(
             "id" => $user[0]["id"],
             "email" => $user[0]["email"],
@@ -16,6 +17,7 @@ if ( isset($_COOKIE[$cookieSession]) && !empty($_COOKIE[$cookieSession]) ){
         );
 		$_SESSION[$cookieSession] = $user[0]["email"];	
 	}else{
+        $_SESSION['valid'] = false;
 		header("Location: index.php?v=Home&error=" . md5(rand(0000,9999)));die();
 	}
 }
