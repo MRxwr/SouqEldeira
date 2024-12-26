fcf3f13da22ec9e709ecff75406982724bb2b47b
MRxwr <nasserhatab@gmail.com>
2024-12-26 09:05:51 +0300

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -6,6 +6,6 @@ require_once ("../admin/includes/translate.php");
 setcookie($cookieSession, "", time() - (86400*30 ), "/");
 session_start ();
 if ( session_destroy() ){
-	header("Location: index.php?v=Home");
+	header("Location: ../index.php?v=Home");
 }
 ?>
