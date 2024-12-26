28f2607e4f638700dd591cd3f52b38af2a70452c
MRxwr <nasserhatab@gmail.com>
2024-12-26 09:26:34 +0300

    Update bladeLogout.php

--- views/bladeLogout.php
+++ views/bladeLogout.php
@@ -1,11 +1,3 @@
 <?php
-   session_start();
-   unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
-   unset($_SESSION["time"]);
-   
-   $_SESSION["valid"] = false; 
-   
-   echo 'You have cleaned session';
-   header('Refresh: 2; URL = index.php');
+header("Location: includes/logout.php");
 ?>
