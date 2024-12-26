b262111ea541e74bd3dc48951df531b9cd099ef7
MRxwr <nasserhatab@gmail.com>
2024-12-26 08:28:28 +0300

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -3,8 +3,7 @@ include_once ("includes/config.php");
 require("includes/translate.php");
 setcookie($cookieSession."A", "", time() - (86400*30 ), "/");
 session_start ();
-if ( session_destroy() )
-{
+if ( session_destroy() ){
 	header("Location: login.php");
 }
 ?>
