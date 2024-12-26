7e218e44c0332aaaa0bd3f54e14786965ae123ad
MRxwr <nasserhatab@gmail.com>
2024-12-26 09:46:19 +0300

    Update bladeLogin.php

--- views/bladeLogin.php
+++ views/bladeLogin.php
@@ -1,7 +1,7 @@
 <?php
 if ( isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["password"]) ){
-	var_dump($_POST["username"]);
 	if( $users = selectDBNew("users",[$_POST["username"],sha1($_POST["password"])],"`username` LIKE ? AND `password` LIKE ?","") ){
+		var_dump($_POST["username"]);
 		if( $users[0]["status"] != 0 ){
 			$msg = direction("Your account is blocked", "تم حظر حسابك");
 		}
