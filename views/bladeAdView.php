1a160460cc2f03224c400429ea814b154adbfb85
MRxwr <nasserhatab@gmail.com>
2024-12-27 01:05:17 +0300

    Update bladeAdView.php

--- views/bladeAdView.php
+++ views/bladeAdView.php
@@ -2,6 +2,7 @@
 if( isset($_GET['id']) && $ad = selectDBNew("products",[$_GET['id']],"`id` = ?","") ){
 	$area = selectDBNew("areas",[$ad[0]['areaId']],"`id` = ?","");
 	updateDB("products",["views" => $ad[0]['views'] + 1],"`id` = {$_GET["id"]}");
+	$ad[0]["views"] = $ad[0]['views'] + 1;
 }else{
 	?>
 	<script>
