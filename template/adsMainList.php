488e29f29e267f0cc2491600e39ab5d2acd11210
MRxwr <nasserhatab@gmail.com>
2024-12-27 00:36:55 +0300

    Update adsMainList.php

--- template/adsMainList.php
+++ template/adsMainList.php
@@ -3,7 +3,6 @@
 	<?php
 	if( $ads = selectDB("products","`status` = '0' AND `hidden` != '2' ORDER BY `id` DESC LIMIT 3") ){
 		for( $i = 0; $i < sizeof($ads); $i++ ){
-			/*
 			if( $ads[$i]["packageId"] == 1 ){
 				$feature = "card-feature";
 			}else{
@@ -11,7 +10,7 @@
 			}
 	?>
 <div class="col-lg-12">
-	<a style="cursor: pointer" class="card card-ad <?php echo $feature; ?>" <?php /*data-bs-toggle="modal" data-bs-target="#ad_modal" ?> href="?v=AdView&id=<?php echo $ads[$i]['id']; ?>">
+	<a style="cursor: pointer" class="card card-ad <?php echo $feature; ?>" <?php /*data-bs-toggle="modal" data-bs-target="#ad_modal" */?> href="?v=AdView&id=<?php echo $ads[$i]['id']; ?>">
 		<div class="row g-0"> 
 			<div class="col-4 col-sm-3 position-relative">
 				<span class="feature-label"><?php echo Trans('app','Feature'); ?></span> 
@@ -76,7 +75,6 @@
 	</a>
 </div>
 			<?php
-			*/
 		}
 	}
 	?>
