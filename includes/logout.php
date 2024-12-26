4ae4e71bba92417e812baa16f72f11840cd58448
MRxwr <nasserhatab@gmail.com>
2024-12-26 08:58:16 +0300

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -1,7 +1,7 @@
 <?php
-include_once ("includes/config.php");
-include_once ("includes/functions.php");
-require("includes/translate.php");
+include_once ("admin/includes/config.php");
+include_once ("admin/includes/functions.php");
+require_once ("admin/includes/translate.php");
 setcookie($cookieSession, "", time() - (86400*30 ), "/");
 session_start ();
 if ( session_destroy() ){
