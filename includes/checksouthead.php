976d56ff1ea5b1b64023af8b3becde087ed67060
MRxwr <nasserhatab@gmail.com>
2024-12-26 13:14:12 +0300

    Update checksouthead.php

--- includes/checksouthead.php
+++ includes/checksouthead.php
@@ -9,7 +9,7 @@ if ( isset($_COOKIE[$cookieSession]) && !empty($_COOKIE[$cookieSession]) ){
 	if ( $user = selectDBNew("users", [$svdva], "`keepMeAlive` LIKE ?", "")){
         if( $user[0]["status"] != 0 ){
             $_SESSION['valid'] = false;
-            header("Location: index.php?v=Logout");die();
+            header("Location: index.php?v=Home&error=status");die();
         }
         if( $user[0]["hidden"] != 0 ){
             $_SESSION['valid'] = false;
