9e83f95afe25da7fd563605a00892d70e5686834
MRxwr <nasserhatab@gmail.com>
2024-12-26 10:04:43 +0300

    Update bladeLogin.php

--- views/bladeLogin.php
+++ views/bladeLogin.php
@@ -12,7 +12,7 @@ if ( isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["pa
 			$msg = direction("Wrong username or password", "اسم المستخدم او كلمة المرور غير صحيحة");
 		}else{
 			$GenerateNewCC = md5(rand());
-			var_dump($users[0]["id"]);
+			var_dump(updateDB("users",array("keepMeAlive"=>$GenerateNewCC),"`id` = '{$users[0]["id"]}'"));
 			if( updateDB("users",array("keepMeAlive"=>$GenerateNewCC),"`id` = '{$users[0]["id"]}'") ){
 				var_dump($GenerateNewCC);
 				$_SESSION[$cookieSession] = $email;
