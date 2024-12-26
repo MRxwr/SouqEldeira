0c82043e8d76089cf865c16fc8fb54c8164e29ce
MRxwr <nasserhatab@gmail.com>
2024-12-26 12:33:39 +0300

    save

--- views/bladeLogin.php
+++ views/bladeLogin.php
@@ -40,7 +40,6 @@ if( isset($_POST["register"]) && !empty($_POST["username"]) && !empty($_POST["na
 				"password"	=>	$password,
 				"keepMeAlive"	=>	$GenerateNewCC,
 			);
-			var_dump($data);
 			if( insertDB("users",$data) ){
 				$_SESSION["timeout"] = time() + (86400*30);
 				$_SESSION[$cookieSession] = $_POST["email"];
