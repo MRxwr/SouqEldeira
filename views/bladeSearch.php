c2a5e48af9baa99c1af07017b832f3df1a1d5416
MRxwr <nasserhatab@gmail.com>
2024-12-29 12:14:30 +0300

    Update bladeSearch.php

--- views/bladeSearch.php
+++ views/bladeSearch.php
@@ -1,6 +1,44 @@
 <?php
-if( $category = selectDBNew("categories",[$_POST["categoryId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
-	$categoyTitle = direction($category[0]["enTitle"],$category[0]["arTitle"]);
+if ( isset($_POST) ){
+	if( $category = selectDBNew("categories",[$_POST["categoryId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
+		$categoyTitle = direction($category[0]["enTitle"],$category[0]["arTitle"]);
+	}else{
+		?>
+		<script>
+			alert('<?php echo direction("Could not process your request, Please try again","حدث خطأ أثناء عملية الطلب, يرجى المحاولة مرة أخرى"); ?>');
+			window.location.href = 'index.php?v=Home';
+		</script>
+		<?php
+	}
+	if( $area = selectDBNew("areas",[$_POST["areaId"]],"`status` = '0' AND `hidden` = '0' AND `id` = ?","") ){
+		$areaTitle = direction($area[0]["enTitle"],$area[0]["arTitle"]);
+	}else{
+		?>
+		<script>
+			alert('<?php echo direction("Could not process your request, Please try again","حدث خطأ أثناء عملية الطلب, يرجى المحاولة مرة أخرى"); ?>');
+			window.location.href = 'index.php?v=Home';
+		</script>
+		<?php
+	}
+	if( isset($_POST["from"]) && !empty($_POST["from"]) && isset($_POST["to"]) && !empty($_POST["to"]) ){
+		$price = " AND `price` BETWEEN '{$_POST["from"]}' AND '{$_POST["to"]}' ";
+	}elseif( isset($_POST["from"]) && !empty($_POST["from"]) ){
+		$price = " AND `price` >= '{$_POST["from"]}' ";
+	}elseif( isset($_POST["to"]) && !empty($_POST["to"]) ){
+		$price = " AND `price` <= '{$_POST["to"]}' ";
+	}else{
+		$price = "";
+	}
+	if( isset($_POST["propertyType"]) && !empty($_POST["propertyType"]) ){
+		$propertyType = " AND `propertyType` = '{$_POST["propertyType"]}' ";
+	}else{
+		$propertyType = "";
+	}
+	if( $ads = selectDBNew("products",[$_POST["categoryId"],$_POST["areaId"]],"`status` = '0' AND `hidden` != '2' AND `categoryId` = ? AND `areaId` = ? {$price} {$propertyType} ORDER BY `packageId` DESC,`id` DESC","") ){
+	}
+}elseif( isset($_GET["search"]) && !empty($_GET["search"]) ){
+	if( $ads = selectDBNew("products",[$_GET["search"]],"`status` = '0' AND `hidden` != '2' AND `categoryId` = ? ORDER BY `packageId` DESC,`id` DESC","") ){
+	}
 }else{
 	?>
 	<script>
@@ -8,32 +46,6 @@ if( $category = selectDBNew("categories",[$_POST["categoryId"]],"`status` = '0' 
 		window.location.href = 'index.php?v=Home';
 	</script>
 	<?php
-}
-if( $area = selectDBNew("areas",[$_POST["areaId"]],"`status` = '0' AND `hidden` = '0' AND `id` = ?","") ){
-	$areaTitle = direction($area[0]["enTitle"],$area[0]["arTitle"]);
-}else{
-	?>
-	<script>
-		alert('<?php echo direction("Could not process your request, Please try again","حدث خطأ أثناء عملية الطلب, يرجى المحاولة مرة أخرى"); ?>');
-		window.location.href = 'index.php?v=Home';
-	</script>
-	<?php
-}
-if( isset($_POST["from"]) && !empty($_POST["from"]) && isset($_POST["to"]) && !empty($_POST["to"]) ){
-	$price = " AND `price` BETWEEN '{$_POST["from"]}' AND '{$_POST["to"]}' ";
-}elseif( isset($_POST["from"]) && !empty($_POST["from"]) ){
-	$price = " AND `price` >= '{$_POST["from"]}' ";
-}elseif( isset($_POST["to"]) && !empty($_POST["to"]) ){
-	$price = " AND `price` <= '{$_POST["to"]}' ";
-}else{
-	$price = "";
-}
-if( isset($_POST["propertyType"]) && !empty($_POST["propertyType"]) ){
-	$propertyType = " AND `propertyType` = '{$_POST["propertyType"]}' ";
-}else{
-	$propertyType = "";
-}
-if( $ads = selectDBNew("products",[$_POST["categoryId"],$_POST["areaId"]],"`status` = '0' AND `hidden` != '2' AND `categoryId` = ? AND `areaId` = ? {$price} {$propertyType} ORDER BY `packageId` DESC,`id` DESC","") ){
 }
 ?>
 <div class="search-ads-options mb-3">
