94638e2a348bb8f0524d8f87702db451b972d3d2
MRxwr <nasserhatab@gmail.com>
2024-12-29 12:08:19 +0300

    Update bladeSearch.php

--- views/bladeSearch.php
+++ views/bladeSearch.php
@@ -1,5 +1,4 @@
 <?php
-var_dump($_POST);
 if( $category = selectDBNew("categories",[$_POST["categoryId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
 	$categoyTitle = direction($category[0]["enTitle"],$category[0]["arTitle"]);
 }else{
