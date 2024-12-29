9b6454da3f454e1b143f70397adeaf7391bb7a7d
MRxwr <nasserhatab@gmail.com>
2024-12-29 12:21:08 +0300

    Update bladeSearch.php

--- views/bladeSearch.php
+++ views/bladeSearch.php
@@ -1,5 +1,5 @@
 <?php
-if ( isset($_POST) ){
+if ( isset($_POST) && isset($_POST["categoryId"]) && !empty($_POST["categoryId"]) ){
 	if( $category = selectDBNew("categories",[$_POST["categoryId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
 		$categoyTitle = direction($category[0]["enTitle"],$category[0]["arTitle"]);
 	}else{
