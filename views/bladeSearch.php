971936a163f45125be3ba1e325bea34bfddac776
MRxwr <nasserhatab@gmail.com>
2024-12-29 11:50:42 +0300

    Update bladeSearch.php

--- views/bladeSearch.php
+++ views/bladeSearch.php
@@ -1,32 +1,62 @@
+<?php
+var_dump($_POST);
+if( $category = selectDBNew("category",[$_POST["categoryId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
+	$categoyTitle = direction($category[0]["enTitle"],$category[0]["arTitle"]);
+}else{
+	?>
+	<script>
+		alert('<?php echo direction("Could not process your request, Please try again","حدث خطأ أثناء عملية الطلب, يرجى المحاولة مرة أخرى"); ?>');
+		window.location.href = 'index.php?v=Home';
+	</script>
+	<?php
+}
+if( $area = selectDBNew("areas",[$_POST["areaId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","") ){
+	$areaTitle = direction($area[0]["enTitle"],$area[0]["arTitle"]);
+}else{
+	?>
+	<script>
+		alert('<?php echo direction("Could not process your request, Please try again","حدث خطأ أثناء عملية الطلب, يرجى المحاولة مرة أخرى"); ?>');
+		window.location.href = 'index.php?v=Home';
+	</script>
+	<?php
+}
+if( isset($_POST["from"]) && !empty($_POST["from"]) && isset($_POST["to"]) && !empty($_POST["to"]) ){
+	$price = " AND `price` BETWEEN '{$_POST["from"]}' AND '{$_POST["to"]}' ";
+}elseif( isset($_POST["from"]) && !empty($_POST["from"]) ){
+	$price = " AND `price` >= '{$_POST["from"]}' ";
+}elseif( isset($_POST["to"]) && !empty($_POST["to"]) ){
+	$price = " AND `price` <= '{$_POST["to"]}' ";
+}else{
+	$price = "";
+}
+if( $ads = selectDBNew("products",[$_POST["categoryId"],$_POST["areaId"]],"`status` = '0' AND `hidden` != '2' AND `categoryId` = ? AND `areaId` = ? {$price} ORDER BY `packageId` DESC,`id` DESC","") ){
+}
+?>
+<div class="search-ads-options mb-3">
+	<div class="div">
+		<span class="title"><?php echo Trans('app','Search Options'); ?></span>
+	</div> 
+	<div class="div">
+		<span class="title"><?php echo Trans('app','Type'); ?></span>
+		<span class="data">
+			<span><?php echo $categoyTitle; ?><span> 
+		</span>
+	</div>
+	<div class="div">
+		<span class="title"><?php echo Trans('app','Region'); ?></span>
+		<span class="data">
+			<span><?php echo $areaTitle; ?></span>
+		</span>
+	</div>
+</div>
 
-        	
-	        <div class="search-ads-options mb-3">
-	        	<div class="div">
-	        		<span class="title"><?php echo Trans('app','Search Options'); ?></span>
-	        	</div> 
-	        	<div class="div">
-	        		<span class="title"><?php echo Trans('app','Type'); ?></span>
-	        		<span class="data">
-	        			<span><?php echo Trans('app','Sale'); ?><span> 
-	        		</span>
-	        	</div>
-	        	<div class="div">
-	        		<span class="title"><?php echo Trans('app','Region'); ?></span>
-	        		<span class="data">
-	        			<span><?php echo Trans('app','Khiran'); ?></span>
-	        			<span><?php echo Trans('app','Khiran'); ?></span>
-	        			<span><?php echo Trans('app','Khiran'); ?></span>
-	        		</span>
-	        	</div>
-	        </div>
-			
-			<div class="search-title mb-3 mt-2">  
-				<h4><i class="bi bi-search"></i><?php echo Trans('app','Search Result'); ?><span>(115 <?php echo Trans('app','Ad'); ?>)</span></h4>
-			</div>
-			
-			<div class="ads-section">
-			  <?php include 'template/adsMainList.php'; ?>
-			  <div class="d-block text-end mt-3">
-			  	<a href="" class="btn btn-primary"><?php echo Trans('app','More'); ?> ...</a>
-			  </div>
-			</div>
+<div class="search-title mb-3 mt-2">  
+	<h4><i class="bi bi-search"></i><?php echo Trans('app','Search Result'); ?><span>(115 <?php echo Trans('app','Ad'); ?>)</span></h4>
+</div>
+
+<div class="ads-section">
+	<?php include 'template/adsMainList.php'; ?>
+	<div class="d-block text-end mt-3">
+	<a href="" class="btn btn-primary"><?php echo Trans('app','More'); ?> ...</a>
+	</div>
+</div>
