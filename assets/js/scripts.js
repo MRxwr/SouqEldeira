545069e573956d3b9d669aeacf93c911cbaf1851
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-12-30 01:20:55 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
 	    });
 	});
     // Handle file selection and preview for all file inputs
-    $('input[type="files"]').on('change', function (event) {
+    $('input.fileInput').on('change', function (event) {
       // Get the file input ID
       const fileInputId = $(this).attr('id');
       // Map to the corresponding span ID
