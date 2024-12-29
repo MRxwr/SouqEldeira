d79bf3e74042193049f3ff9a481ce62f61265313
MRxwr <nasserhatab@gmail.com>
2024-12-30 00:34:17 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -4,6 +4,7 @@
         $date = date("Y-m-d");
         $myads = selectDB("products"," `expiryDate` > '{$date}' AND `status` = '0' AND `userId` = '{$id}'");
     }else{
+        $date = date("Y-m-d");
         $myads = selectDB("products"," `expiryDate` < '{$date}' AND `status` = '0' AND `userId` = '{$id}'");
     }
     return $myads;
