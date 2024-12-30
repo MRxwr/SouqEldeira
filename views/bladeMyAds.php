4a0d78ba02f1c6ebaf794bed282a6857c0f374d0
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-12-30 16:10:01 +0530

    Update bladeMyAds.php

--- views/bladeMyAds.php
+++ views/bladeMyAds.php
@@ -208,8 +208,8 @@
 									$feature = "";
 								}
 								$area = selectDB("areas","`id` = '".$ad['areaId']."'")[0];
-								//$gov = selectDB("governorates","`id` = '".$area['governateId']."'")[0];
-								var_dump($area['governateId']);
+								$gov = selectDB("governates","`id` = '".$area['governateId']."'")[0];
+								//var_dump($gov);
 								?>
 								
 								<?php
@@ -240,7 +240,7 @@
 										
 										<div class="data">
 											<h4><?php echo direction($ad['enTitle'],$ad['arTitle']); ?></h4>
-											<h5><?php// echo direction($gov['enTitle'],$gov['arTitle']); ?> - <?php echo direction($area['enTitle'],$area['arTitle']); ?></h5>
+											<h5><?php echo direction($gov['enTitle'],$gov['arTitle']); ?> - <?php echo direction($area['enTitle'],$area['arTitle']); ?></h5>
 											<h6><?php echo Trans('app','Created Date'); ?> &nbsp;&nbsp; 
 											<?php
 											$adDate = new DateTime($ad['date']);
