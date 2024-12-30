89e3e78f1e3c0ac823291d5756b4f428f8ab192e
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-12-30 16:46:05 +0530

    Update bladeMyAds.php

--- views/bladeMyAds.php
+++ views/bladeMyAds.php
@@ -11,7 +11,7 @@
 	$specialAds = $user[0]["specialAd"];
 	$order = selectDB("orders2","`userId` = '{$user[0]["id"]}' ORDER BY `id` DESC LIMIT 1","");
 	$package = selectDBNew("packages",[$order[0]["packageId"]],"`status` = '0' AND `hidden` = '1' AND `id` = ?","");
-	var_dump($package);
+	var_dump($order);
 	if( isset($_GET["republish"]) && !empty($_GET["republish"]) ){
 		$product = selectDB("products","`id` = '{$_GET["republish"]}'")[0];
 		if( ($product["adType"] == 1 && $user[0]["normalAd"] > 1 ) || ($product["adType"] == 2 && $user[0]["specialAd"] > 0 )){	
