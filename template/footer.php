24a5d925256907534355a6c8c0b55a0dfe178020
MRxwr <nasserhatab@gmail.com>
2024-12-31 11:41:59 +0300

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -126,12 +126,11 @@
 							$sMediaArray = ["facebook", "twitter", "instagram", "email", "mobile"];
 							$sMediaIcons = ["bi bi-facebook","bi bi-twitter-x","bi bi-instagram","bi bi-envelope", "bi bi-telephone-fill"];
 							$sMediaLinks = ["https://www.facebook.com/","https://www.twitter.com/","https://www.instagram.com/","mailto:", "tel:"];
-							var_dump($sMedia);
 							for ($i = 0; $i < count($sMediaArray); $i++) {
-								echo $sMedia[$i]["$sMediaArray[$i]"];
-								/*if( !empty($sMedia[$i]["$sMediaArray[$i]"]) ){
-								echo '<li><a href="' . $sMediaLinks[$i] . $sMedia[$i]["$sMediaArray[$i]"] . '"><i class="' . $sMediaIcons[$i] . '"></i></a></li>';
-								}*/
+								echo $sMedia[0]["$sMediaArray[$i]"];
+								if( !empty($sMedia[0]["$sMediaArray[$i]"]) ){
+								echo '<li><a href="' . $sMediaLinks[$i] . $sMedia[0]["$sMediaArray[$i]"] . '"><i class="' . $sMediaIcons[$i] . '"></i></a></li>';
+								}
 							}
 						}
 						?>
