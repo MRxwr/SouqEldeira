01977dcb9906382540aa6d2cae1d9966f5c5595b
MRxwr <nasserhatab@gmail.com>
2025-01-05 13:54:38 +0300

    Update apiFavo.php

--- requests/views/apiFavo.php
+++ requests/views/apiFavo.php
@@ -3,8 +3,12 @@ if( isset($_GET["valid"]) && !empty($_GET["valid"]) ){
     if( $user = selectDBNew("users", [$_GET["valid"]], "`keepMeAlive` LIKE ?","") ){
         $userDetails["id"] = $user[0]["id"];
         if( $product = selectDBNew("products", [$_GET["id"]], "`id` = ?","") ){
-            if( in_array($userDetails["id"],json_decode($product[0]["listOfUsers"],true)) ){
-                $listOfUsers = json_decode($product[0]["listOfUsers"],true);
+            $listOfUsers = json_decode($product[0]["listOfUsers"],true);
+            if( !is_null($listOfUsers) && !is_array($listOfUsers) ){
+                $listOfUsers = array();
+            }
+            if( in_array($userDetails["id"],$listOfUsers) ){
+                
                 unset($listOfUsers[array_search($userDetails["id"],$listOfUsers)]);
                 $listOfUsers = array_values($listOfUsers);
                 $listOfUsers = json_encode($listOfUsers);
