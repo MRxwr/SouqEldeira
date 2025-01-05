bea80cf6a7d68d7d17621f88482737bbb3a7c349
Sonjoy Bhadra <sbhadra0@gmail.com>
2025-01-05 19:10:00 +0530

    Update bladePayment.php

--- views/bladePayment.php
+++ views/bladePayment.php
@@ -62,7 +62,7 @@
 					<div class="form-outline mb-4">
 						<?php if(isset($_GET["result"]) && $_GET["result"] == "CAPTURED"){
 							if($_GET["requested_order_id"]){
-								$orderId  = str_replace('?', '', $_GET["requested_order_id"]);
+								echo $orderId  = $_GET["requested_order_id"];
 								$order = selectDB("orders2"," `orderId` = '$orderId}'","");
 								var_dump($order);
 								if($order && $order[0]["status"] == "0"){
