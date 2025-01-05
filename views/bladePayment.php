2822c3ccf456b905f36f03ac5880f37bb7b34cb8
Sonjoy Bhadra <sbhadra0@gmail.com>
2025-01-05 19:15:06 +0530

    Update bladePayment.php

--- views/bladePayment.php
+++ views/bladePayment.php
@@ -67,6 +67,7 @@
 								//var_dump($order);
 								if($order && $order[0]["status"] == "0"){
 									$package = selectDB("packages","`id` = '{$order[0]["packageId"]}' ORDER BY `id` DESC LIMIT 1","");
+									var_dump($order);
 									if($package){
 										$user = selectDB("users","`id` = '{$order[0]["userId"]}' ORDER BY `id` DESC LIMIT 1","");
 										$normalAds = $user[0]["normalAd"] + $package[0]["quantity"]; //normalAds
