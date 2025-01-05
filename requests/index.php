51c79d4ef247b4917164014ebb613a582e9892a6
MRxwr <nasserhatab@gmail.com>
2025-01-05 13:51:26 +0300

    Update index.php

--- requests/index.php
+++ requests/index.php
@@ -1,7 +1,5 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+require_once '../admin/includes/config.php';
 require_once '../admin/includes/functions.php';
 
 // get viewed page from pages folder \\
