926b12309744d5f8155bc83dc35ee105c3f0f3be
Sonjoy Bhadra <sbhadra0@gmail.com>
2025-01-05 18:19:58 +0530

    Update bladePayment.php

--- views/bladePayment.php
+++ views/bladePayment.php
@@ -63,7 +63,7 @@
 						<?php if(isset($_GET["success"]) && $_GET["success"] == "1"){
 							if($_GET["orderId"]){
 								$orderId = str_replace('?', '', $_GET["orderId"]);
-								$order = selectDB("orders2"," `orderId` = '{$orderId}' ORDER BY `id` DESC LIMIT 1","");
+								$order = selectDB("orders2"," `orderId` LIKE '2025010580541736070669'","");
 								var_dump($order);
 								if($order && $order[0]["status"] == "0"){
 									$package = selectDB("packages","`id` = '{$order[0]["packageId"]}' ORDER BY `id` DESC LIMIT 1","");
