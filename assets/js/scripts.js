d5b91a0c61af015130f1a0f7ab06b027e91bdc46
MRxwr <nasserhatab@gmail.com>
2025-01-07 01:33:27 +0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,8 +12,8 @@ $(document).ready(function () {
     .done(function(response) {
       const responseObject = JSON.parse(response);
       var elem = $(".favo"+id);
-      console.log(responseObject.error);
-      if( responseObject.error == "0" ){
+      console.log(responseObject.data.tag);
+      if( responseObject.data.tag == "0" ){
         elem.attr("style", "color:#000");
       }else{
         elem.attr("style", "color:red");
