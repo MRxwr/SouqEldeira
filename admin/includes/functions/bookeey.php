c6b0aa0f0438af15a195646972ed69140095e65e
MRxwr <nasserhatab@gmail.com>
2026-05-25 00:42:00 +0300

    Update bookeey.php

--- admin/includes/functions/bookeey.php
+++ admin/includes/functions/bookeey.php
@@ -657,6 +657,9 @@ class bookeey {
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $decodeOutput = json_decode($serverOutput, true);
         curl_close ($ch);
+        var_dump(json_encode($postParams));
+        var_dump($serverOutput);
+        die();
 
         if ($serverOutput === false) {
             echo "CURL Error: " . $curlError;
