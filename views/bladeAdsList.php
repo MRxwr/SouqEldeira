714e0871d3967a447fc29d44144f20ed235e3c4c
MRxwr <nasserhatab@gmail.com>
2026-07-11 03:48:37 +0300

    save

--- views/bladeAdsList.php
+++ views/bladeAdsList.php
@@ -1,12 +1,7 @@
-
-	        	
-		       
-				<div class="listings-section">
-					
-				  <h4 class="mb-md-5 mb-3 text-center"><?php echo direction("Ads List","قائمة الإعلانات"); ?></h4>
-				  <?php include 'themes/'.$config['theme'].'/sections/ads-main-list.php'; ?>
-				  <div class="d-block text-end mt-3">
-				  	<a href="" class="btn btn-primary"><?php echo direction("More","المزيد"); ?> <i class="bi bi-three-dots"></i></a>
-				  </div>
-				  
-				</div>
+<div class="listings-section">
+	<h4 class="mb-md-5 mb-3 text-center"><?php echo direction("Ads List","قائمة الإعلانات"); ?></h4>
+	<?php include 'themes/'.$config['theme'].'/sections/ads-main-list.php'; ?>
+	<div class="d-block text-end mt-3">
+	<a href="" class="btn btn-primary"><?php echo direction("More","المزيد"); ?> <i class="bi bi-three-dots"></i></a>
+	</div>
+</div>
