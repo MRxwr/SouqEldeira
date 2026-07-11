e1a13cf26b193f1bf07804bd22a8916fa54efa47
MRxwr <nasserhatab@gmail.com>
2026-07-12 01:06:52 +0300

    Update header.php

--- template/header.php
+++ template/header.php
@@ -47,7 +47,7 @@ $ogUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'h
         <meta name="description" content="<?php echo htmlspecialchars(strip_tags($ogDescription)); ?>" />
         <meta name="author" content="" />
         <title><?php echo htmlspecialchars($ogTitle); ?></title>
-
+        <link rel="canonical" href="<?php echo "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}" ?>" />
         <link rel="alternate" hreflang="ar-KW" href="<?php echo "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}" . getSign() . "lang=ar"; ?>" />
         <link rel="alternate" hreflang="en-KW" href="<?php echo "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}" . getSign() . "lang=en"; ?>" />
         <link rel="alternate" hreflang="x-default" href="<?php echo "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}" . getSign() . "lang=en"; ?>" />
