7a6a0705397a034f4c651aeb41c806e1cf22ac2b
MRxwr <nasserhatab@gmail.com>
2026-07-12 03:03:36 +0300

    Update header.php

--- template/header.php
+++ template/header.php
@@ -2,7 +2,7 @@
 include 'includes/checksouthead.php';
 readSlug();
 // Default OG values
-$ogTitle = direction("Souq Al Deerah | Properties for sale and rent in Kuwait","عقارات للبيع والإيجار في الكويت | سوق الديرة");
+$ogTitle = direction("Properties for sale and rent in Kuwait","عقارات للبيع والإيجار في الكويت");
 $ogDescription = direction("Souq Al Deerah website for ads", "موقع سوق الديرة للإعلانات"); 
 $ogImage = $baseURL . "assets/img/logo-1.png";
 
