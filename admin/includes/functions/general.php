6c1ab7018e69570083dae0cbca5a02337d36853e
MRxwr <nasserhatab@gmail.com>
2026-07-14 11:14:39 +0300

    save

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -177,128 +177,157 @@ function readSlug(){
 
 	switch ($segments[0]) {
 		case 'about':
-			$_GET['v'] = 'About'; 
+			$_GET['v'] = 'About';
+			$pageTitle = direction("About Us","من نحن");
 			break;
 		
 		case 'add-ad':
 			$_GET['v'] = 'AddAd'; 
+			$pageTitle = direction("Add Ad","إضافة إعلان");
 			break;
 
 		case 'edit-ad':
 			$_GET['v'] = 'EditAd'; 
+			$pageTitle = direction("Edit Ad","تحديث الإعلان");
 			$_GET["id"] = (int)$segments[1];
 			break;
 
 		case 'ads-list':
 			$_GET['v'] = 'AdsList'; 
+			$pageTitle = direction("Ads List","قائمة الإعلانات");
+			$_GET["type"] = (int)$segments[1];
 			break;
 
 		case 'ad-view':
 			$_GET['v'] = 'AdView';
+			$pageTitle = "";
 			$_GET['id'] = (int)$segments[1];
 			break;
 
 		case 'contact':
 			$_GET['v'] = 'Contact';
+			$pageTitle = direction("Contact Us","اتصل بنا");
 			break;
 
 		case 'faq':
 			$_GET['v'] = 'FAQ';
+			$pageTitle = direction("FAQ","الاسئلة الشائعة");
 			break;
 
 		case 'home':
 			$_GET['v'] = 'Home';
+			$pageTitle = direction("Home","الصفحة الرئيسية");
 			break;
 
 		case 'login':
 			$_GET['v'] = 'Login';
+			$pageTitle = direction("Login","تسجيل الدخول");
 			break;
 
 		case 'reset-phone':
 			$_GET['v'] = 'Login';
+			$pageTitle = "";
 			$_GET['reset'] = 'phone';
 			break;
 
 		case 'logout':
 			$_GET['v'] = 'Logout';
+			$pageTitle = direction("Logout","تسجيل الخروج");
 			break;
 
 		case 'my-ads':
 			$_GET['v'] = 'MyAds'; 
+			$pageTitle = direction("My Ads","الإعلانات الخاصة بي");
 			break;
 
 		case 'my-ads-list':
 			$_GET['v'] = 'MyAdsList'; 
+			$pageTitle = direction("My Ads List","قائمة الإعلانات الخاصة بي");
 			break;
 
 		case 'my-ads-list-type':
 			$_GET['v'] = 'MyAdsList'; 
+			$pageTitle = direction("My Ads List","قائمة الإعلانات الخاصة بي");
 			$_GET["type"] = "{$segments[1]}" ?? '';
 			$_GET["page"] = (int)($segments[2] ?? 1);
 			break;
 
 		case 'my-ads-error':
 			$_GET['v'] = 'MyAds'; 
+			$pageTitle = direction("My Ads","الإعلانات الخاصة بي");
 			$_GET["error"] = (int)$segments[1];
 			break;
 
 		case 'my-ads-delete':
 			$_GET['v'] = 'MyAds'; 
+			$pageTitle = direction("My Ads","الإعلانات الخاصة بي");
 			$_GET["forceDelete"] = (int)$segments[1];
 			break;
 
 		case 'my-ads-remove':
 			$_GET['v'] = 'MyAds'; 
+			$pageTitle = direction("My Ads","الإعلانات الخاصة بي");
 			$_GET["remove"] = (int)$segments[1];
 			break;
 
 		case 'my-ads-success':
 			$_GET['v'] = 'MyAds'; 
+			$pageTitle = direction("My Ads","الإعلانات الخاصة بي");
 			$_GET["success"] = (int)$segments[1];
 			break;
 
 		case 'notifications':
 			$_GET['v'] = 'Notifications'; 
+			$pageTitle = direction("Notifications","الإشعارات");
 			break;
 
 		case 'offices':
 			$_GET['v'] = 'Offices'; 
+			$pageTitle = direction("Offices","المكاتب");
 			break;
 
 		case 'office-view':
 			$_GET['v'] = 'OfficeView';
+			$pageTitle = "";
 			$_GET['id'] = (int)$segments[1];
 			break;
 
 		case 'payment':
-			$_GET['v'] = 'Payment'; 
+			$_GET['v'] = 'Payment';
+			$pageTitle = direction("Payment","الدفع");
 			break;
 
 		case 'payment-error':
 			$_GET['v'] = 'Payment'; 
+			$pageTitle = direction("Payment Error","خطأ في الدفع");
 			$_GET["error"] = "1";
 			break;
 
 		case 'policy':
 			$_GET['v'] = 'Policy'; 
+			$pageTitle = direction("Privacy Policy","سياسة الخصوصية");
 			break;
 
 		case 'profile':
 			$_GET['v'] = 'Profile'; 
+			$pageTitle = direction("Profile","الملف الشخصي");
 			break;
 			
 		case 'search':
 			$_GET['v'] = 'Search';
+			$pageTitle = direction("Search","بحث");
 			$_GET['title'] = $segments[1] ?? '';
 			$_GET['type'] = $segments[2] ?? '';
 			break;
 
 		case 'terms':
 			$_GET['v'] = 'Terms'; 
+			$pageTitle = direction("Terms & Conditions","الشروط والأحكام");
 			break;
 
 		case '':
 			$_GET['v'] = 'Home';
+			$pageTitle = direction("Home","الصفحة الرئيسية");
 			break;
 	}
 }
