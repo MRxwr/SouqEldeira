11a8f14f0060b39f8dc24553892d22b79d7d96fa
MRxwr <nasserhatab@gmail.com>
2026-08-12 01:37:02 +0300

    Update bladeNewsView.php

--- views/bladeNewsView.php
+++ views/bladeNewsView.php
@@ -13,15 +13,32 @@ if( isset($_GET['id']) && $news = selectDBNew("news",[$_GET['id']],"`id` = ? AND
 
 <div class="ad-details">
 	<div class="row">
-		<div class="col-12 mb-3">
-			<h1 class="fw-bold"><?php echo direction($news[0]['enTitle'], $news[0]['arTitle']); ?></h1>
+		<div class="col-md-8 col-sm-12 mx-auto">
+			<div class="row">
+				<div class="col-12 mb-3">
+					<h1 class="fw-bold"><?php echo direction($news[0]['enTitle'], $news[0]['arTitle']); ?></h1>
+				</div>
+				<div class="col-sm-12">
+					<img src="/logos/<?php echo $news[0]['imageurl']; ?>" class="img-fluid w-100" style="object-fit: cover; height: 300px; border-radius: 10px;" alt="<?php echo direction($news[0]['enTitle'], $news[0]['arTitle']); ?>">
+				</div>
+				<div class="col-sm-12">
+					<h5 class="fw-bold"><?php echo direction("Description","الوصف"); ?></h5>
+					<p><?php echo direction($news[0]['enDetails'], $news[0]['arDetails']); ?></p>
+				</div>
+			</div>
 		</div>
-		<div class="col-sm-12">
-			<img src="/logos/<?php echo $news[0]['imageurl']; ?>" class="img-fluid w-100" style="object-fit: cover; height: 300px; border-radius: 10px;" alt="<?php echo direction($news[0]['enTitle'], $news[0]['arTitle']); ?>">
-		</div>
-		<div class="col-sm-12">
-			<h5 class="fw-bold"><?php echo direction("Description","الوصف"); ?></h5>
-			<p><?php echo direction($news[0]['enDetails'], $news[0]['arDetails']); ?></p>
+		<div class="col-md-4 col-sm-12">
+			<div class="listings-section">
+				<h4 class="text-start mt-2 mb-3"><?php echo direction("More News","المزيد من الأخبار"); ?></h4>
+				<?php
+				if( $newsList = selectDB("news","`status` = '0' AND `hidden` = '1' AND `id` != {$news[0]['id']} ORDER BY `id` DESC LIMIT 3") ){
+					include 'template/newsMainList.php';
+				}
+				?>
+				<div class="d-block text-end mt-3">
+					<a href="/news-list/1" class="btn btn-primary"><?php echo direction("More","المزيد"); ?> <i class="bi bi-three-dots"></i></a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -47,14 +64,4 @@ if( isset($_GET['id']) && $news = selectDBNew("news",[$_GET['id']],"`id` = ? AND
 
 <hr>
 
-<div class="listings-section">
-	<h4 class="text-start mt-2 mb-3"><?php echo direction("More News","المزيد من الأخبار"); ?></h4>
-	<?php
-	if( $newsList = selectDB("news","`status` = '0' AND `hidden` = '1' AND `id` != {$news[0]['id']} ORDER BY `id` DESC LIMIT 3") ){
-		include 'template/newsMainList.php';
-	}
-	?>
-	<div class="d-block text-end mt-3">
-		<a href="/news-list/1" class="btn btn-primary"><?php echo direction("More","المزيد"); ?> <i class="bi bi-three-dots"></i></a>
-	</div>
-</div>
+
