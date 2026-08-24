325420ff22002dace9c3dceab13648e9fabca1e5
MRxwr <nasserhatab@gmail.com>
2026-08-25 00:06:32 +0300

    Render page structured data through SchemaBuilder

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 ob_start();
 require_once 'template/header.php';
+require_once 'includes/SchemaBuilder.php';
+
 if( isset($_GET["finalstatus"]) ){
 	if( strtolower(base64_decode($_GET["finalstatus"])) == "success" ){
 		$_GET["v"] = "Payment";
@@ -13,10 +15,14 @@ if( isset($_GET["finalstatus"]) ){
 
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
+	$currentView = $_GET["v"];
 	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
 }else{
+	$currentView = "Home";
 	require_once("views/bladeHome.php");
 }
+
+SchemaBuilder::renderForPage($currentView, get_defined_vars());
 
 require_once 'template/footer.php';
 ob_end_flush();
